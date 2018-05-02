a876b5166e64bb008679ada9537231d7766acf4f
Vishvajit Bakrola <38208499+vbakrola@users.noreply.github.com>
2018-05-02 04:22:45 -0700

    Initial code
    Initial code to establish WiFi connection with ESP8266 module, by initializing module as a server.

---
+++ ESP8266_WiFi_Connection.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+
+//Initializing network SSId PASSWORD as constant character.
+const char* ssid = "GIVE_YOUR_NETWORK_SSID";
+const char* password = "PASSWORD";
+
+//Creates server that listen for incoming connections on the specific port. I've choosen port number 80.
+WiFiServer server(80);
+
+
+void setup() {
+  //Initializing serial communication - Baud rate for me 115200.
+  Serial.begin(115200);
+  delay(10);
+
+  //Connect to WiFi network.
+  Serial.println("Connecting to");
+  Serial.print(".");
+  
+  //Printing network SSID on serial monitor.
+  Serial.println(ssid);
+  
+  //Initializing network settings of WiFi library.
+  WiFi.begin(ssid, password);
+
+  //WiFi.status() returns connection status, funtion returns WL_CONNECTED on successfull connection.
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  //Starting up the server.
+  server.begin();
+  Serial.println("Server started");
+ 
+  //Printing an IP address on serial monitor to be opened on broweser for you problem specific purpose. 
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+
+  //localIP() method retrives IP address of WiFi shield.
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+
+//YOUR LOOP AREA//
+
